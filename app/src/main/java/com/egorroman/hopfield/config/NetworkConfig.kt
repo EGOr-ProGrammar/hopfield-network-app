ca0178b895f15cd46e555a28c262932093961888
EGOr-ProGrammar <need.extra.post@gmail.com>
2026-05-29 20:38:26 +0300

    build: setup dependencies and network configuration

---
+++ app/src/main/java/com/egorroman/hopfield/config/NetworkConfig.kt
@@ -0,0 +1,13 @@
+package com.egorroman.hopfield.config
+
+object NetworkConfig {
+    const val DEFAULT_GRID_SIZE = 5
+    const val MIN_GRID_SIZE = 3
+    const val MAX_GRID_SIZE = 10
+    
+    const val MAX_RECOGNITION_ITERATIONS = 100
+    
+    // Cell states
+    const val STATE_ACTIVE = 1
+    const val STATE_INACTIVE = -1
+}
